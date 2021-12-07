76f5c60b5e419af66beac0d6da8c01f5a59d21a9
Davide <Puccio98>
2021-12-07 10:08:32 +0100

    Added window resize functionality

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QSlider, QFrame, QGraphicsView, QGraphicsScene, QToolButton, QApplication, QMenu, QMenuBar, QWidget, QVBoxLayout, QLineEdit, QColorDialog, QHBoxLayout, QLabel, QPushButton
-from PyQt5.QtGui import QColor, QPen, QBrush, QIcon
+from PyQt5.QtGui import QColor, QPen, QBrush, QIcon, QIntValidator
 from PyQt5.QtCore import Qt
-from model import Checkboard
-
-default_colors = {
-    "Alive": QColor("white"),
-    "Dead": QColor("red"),
-    "Born": QColor("blue")
-}
+from model import CheckboardModel
 
 
 class Toolbar(QHBoxLayout):
@@ -29,86 +23,81 @@ class Toolbar(QHBoxLayout):
         self.setMenuBar(self._menu_bar)
 
 
-class GridSizeInput(QLineEdit):
-    def __init__(self, name, val=30, **kwargs):
-        super().__init__(**kwargs)
-        self._val = val
-        self._name = name
-
-        self.setMaxLength(4)
-        self.setText(str(self._val))
-        self.setMinimumSize(60, 30)
-        self.setMaximumSize(60, 30)
-
-        self.setEnabled(False)
-
-
 class ColorButton(QToolButton):
-    def __init__(self, name, color, **kwargs):
+    def __init__(self, name, model, **kwargs):
         super().__init__(**kwargs)
-        self.set_color(color)
-        self._name = name
+        self.model = model
+        self.name = name
 
-        self.setStyleSheet("background-color: " + self._color)
-
+        self.allignColor()
         self.clicked.connect(self.handle_click)
+        self.model.observeColor(self.allignColor)
 
     def handle_click(self):
-        print('Clicked ', self._name)
         new_color = QColorDialog.getColor()
-        self.set_color(new_color)
+        self.model.setColor(self.name, new_color)
 
-    def set_color(self, new_color):
-        if new_color.isValid():
-            self._color = new_color.name()
-            self.setStyleSheet("background-color: " + self._color)
+    def allignColor(self):
+        color = self.model.getColor(self.name)
+        self.setStyleSheet("background-color: " + color.name())
 
 
 class ConfigPanel(QHBoxLayout):
-    def __init__(self, **kwargs):
+    def __init__(self, model, **kwargs):
         super().__init__(**kwargs)
+        self.model = model
+
+        self.gridSizeInput = QLineEdit(str(self.model.getN()))
+        self.gridSizeInput.setMaxLength(3)
+        self.gridSizeInput.setMinimumSize(50, 30)
+        self.gridSizeInput.setMaximumSize(50, 30)
+        self.gridSizeInput.setValidator(QIntValidator(1, 999))
+        self.gridSizeInput.returnPressed.connect(self.enterNewGridSize)
 
         self.addWidget(QLabel("Grid Size:"), alignment=Qt.AlignLeft)
-        self._grid_size_controllers = []
-        self.addWidget(GridSizeInput("Rows"), alignment=Qt.AlignLeft)
-        self.addWidget(GridSizeInput("Cols"), alignment=Qt.AlignLeft)
+        self.addWidget(self.gridSizeInput, alignment=Qt.AlignLeft)
         self.addStretch()
 
-        self._color_controllers = []
         color_controllers_names = ["Alive", "Dead", "Born"]
         for n in color_controllers_names:
             self.addWidget(QLabel(n + ":"), alignment=Qt.AlignRight)
-            self.addWidget(ColorButton(name=n, color=default_colors[n]), alignment=Qt.AlignLeft)
+            self.addWidget(ColorButton(name=n, model=self.model), alignment=Qt.AlignLeft)
+
+    def enterNewGridSize(self):
+        self.model.setN(int(self.gridSizeInput.text()))
 
 
 class GameGrid(QGraphicsView):
-    def __init__(self, checkboard, rows=60, cols=60, width=600, height=600, **kwargs):
+    def __init__(self, model, width=600, height=600, **kwargs):
         super().__init__(**kwargs)
-        self.checkboardModel = checkboard
-        self.checkboardModel.observe(self.renderBoard)
+        self.model = model
+        self.model.observeBoard(self.renderBoard)
+        self.model.observeColor(self.renderBoard)
+        self.model.observeGridSize(self.changeSizes)
 
         self.width = width
         self.height = height
-        self.cellSizeX = int(self.height / rows)
-        self.cellSizeY = int(self.width / cols)
+        self.cellSize = int(self.height / self.model.getN())
 
         self.scene = QGraphicsScene()
         self.scene.setSceneRect(0, 0, self.width, self.height)
+        self.scene.setBackgroundBrush(QBrush(QColor("black")))
 
-        self.setBackgroundBrush(QBrush(QColor("black")))
         self.setFixedSize(self.width, self.height)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setScene(self.scene)
         self.setFrameStyle(QFrame.NoFrame)
 
+        self.renderBoard()
+
     def eventHandler(self, event):
-        i = int(event.pos().x() / self.cellSizeX)
-        j = int(event.pos().y() / self.cellSizeY)
+        i = int(event.pos().x() / self.cellSize)
+        j = int(event.pos().y() / self.cellSize)
         if(event.buttons() == Qt.LeftButton):
-            self.checkboardModel.addCell(i, j)
+            self.model.addCell(i, j)
         elif(event.buttons() == Qt.RightButton):
-            self.checkboardModel.removeCell(i, j)
+            self.model.removeCell(i, j)
 
     def mousePressEvent(self, event):
         self.eventHandler(event)
@@ -119,21 +108,26 @@ class GameGrid(QGraphicsView):
         return super().mouseMoveEvent(event)
 
     def renderBoard(self):
-        currentBoard = self.checkboardModel.getBoard()
+        currentBoard = self.model.getBoard()
         self.scene.clear()
 
         for indexes, cell in currentBoard.items():
             i = indexes[0]
             j = indexes[1]
-            color = default_colors[cell.getState()]
-            self.scene.addRect(i * self.cellSizeX, j * self.cellSizeY, self.cellSizeX,
-                               self.cellSizeY, QPen(color), QBrush(color))
+            color = self.model.getColor(cell.getState())
+            self.scene.addRect(i * self.cellSize, j * self.cellSize, self.cellSize,
+                               self.cellSize, QPen(color), QBrush(color))
+
+    def changeSizes(self):
+        self.cellSize = int(self.height / self.model.getN())
+        self.renderBoard()
 
 
 class SimulationPanel(QHBoxLayout):
-    def __init__(self, checkboard, **kwargs):
+    def __init__(self, model, **kwargs):
         super().__init__(**kwargs)
-        self.checkboard = checkboard
+        self.model = model
+
         self._left = QPushButton(QIcon("./Icons/iconmonstr-arrow-left.svg"), "")
         self._left.clicked.connect(self.clickLeft)
 
@@ -142,8 +136,13 @@ class SimulationPanel(QHBoxLayout):
 
         self._pause = QPushButton(QIcon("./Icons/iconmonstr-media-control-49.svg"), "")
         self._play = QPushButton(QIcon("./Icons/iconmonstr-media-control-48.svg"), "")
+
         self._speed = QSlider(Qt.Horizontal)
         self._speed.setMaximumSize(300, 50)
+        self._speed.valueChanged[int].connect(self.sliderModified)
+        self._speed.setMinimum(0)
+        self._speed.setMaximum(20)
+        self._speed.setValue(10)
 
         self.addWidget(self._left)
         self.addWidget(self._right)
@@ -154,10 +153,13 @@ class SimulationPanel(QHBoxLayout):
         self.addWidget(self._speed)
 
     def clickRight(self):
-        self.checkboard.next()
+        self.model.next()
 
     def clickLeft(self):
-        self.checkboard.goBack()
+        self.model.goBack()
+
+    def sliderModified(self, value):
+        self.model.setSpeed(value)
 
 
 # The main application.
@@ -166,11 +168,8 @@ class App(QApplication):
     def __init__(self, args):
 
         super().__init__(args)
-        # self.setStyleSheet('*{font-size: 20px; color: white; background-color: #292929;}')
         self.setStyleSheet('*{font-size: 20px;}')
-        rows = 30
-        cols = 30
-        self._model = Checkboard(rows, cols)
+        self._model = CheckboardModel(N=50)
 
         self._root = QWidget()
         self._layout = QVBoxLayout()
@@ -179,9 +178,9 @@ class App(QApplication):
         self._toolbar.setLayout(Toolbar())
 
         self._config = QWidget()
-        self._config.setLayout(ConfigPanel())
+        self._config.setLayout(ConfigPanel(self._model))
 
-        self._game_grid = GameGrid(self._model, rows, cols)
+        self._game_grid = GameGrid(self._model)
 
         self._simulation_panel = QWidget()
         self._simulation_panel.setLayout(SimulationPanel(self._model))
--- model.py
+++ model.py
@@ -1,12 +1,6 @@
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import QObject, pyqtSignal
 
-default_colors = {
-    "Alive": QColor("white"),
-    "Dead": QColor("red"),
-    "Born": QColor("blue")
-}
-
 
 class InterfaceColors():
     def __init__(self):
@@ -16,7 +10,12 @@ class InterfaceColors():
                        }
 
     def setColor(self, key, value):
-        pass
+        assert key in self.colors.keys() and type(value) == QColor
+        self.colors[key] = value
+
+    def getColor(self, key):
+        assert key in self.colors.keys()
+        return self.colors[key]
 
 
 class Cell():
@@ -26,6 +25,7 @@ class Cell():
         self.state = state
 
     def setState(self, state):
+        assert state in ["Alive", "Born", "Dead"]
         self.state = state
 
     def getState(self):
@@ -35,25 +35,56 @@ class Cell():
         return Cell(self.i, self.j, self.state)
 
 
-class Checkboard(QObject):
+class CheckboardModel(QObject):
     boardUpdate = pyqtSignal()
+    colorUpdate = pyqtSignal()
+    gridSizeUpdate = pyqtSignal()
 
-    def __init__(self, rows, cols):
+    def __init__(self, N):
         super().__init__()
         self.boardHistory = []
         self.boardHistory.append({})
         self.currentIndex = 0
-        self.rows = rows
-        self.cols = cols
+        self.N = N
+        self.colors = InterfaceColors()
+        self.speed = 0
 
-    def observe(self, slot):
+    def observeBoard(self, slot):
         self.boardUpdate.connect(slot)
+
+    def observeColor(self, slot):
+        self.colorUpdate.connect(slot)
+
+    def observeGridSize(self, slot):
+        self.gridSizeUpdate.connect(slot)
+
+    def setColor(self, key, value):
+        self.colors.setColor(key, value)
+        self.colorUpdate.emit()
+
+    def getColor(self, key):
+        return self.colors.getColor(key)
+
+    def setN(self, N):
+        assert N > 0
+        self.N = N
+        self.gridSizeUpdate.emit()
+
+    def getN(self):
+        return self.N
+
+    def setSpeed(self, speed):
+        print(speed)
+        self.speed = speed
 
     def getBoard(self):
         return self.boardHistory[self.currentIndex]
 
     def addCell(self, i, j):
         if (i, j) not in self.boardHistory[self.currentIndex].keys():
+            self.boardHistory[self.currentIndex][(i, j)] = Cell(i, j)
+            self.boardUpdate.emit()
+        elif self.boardHistory[self.currentIndex][(i, j)].getState() == "Dead":
             self.boardHistory[self.currentIndex][(i, j)] = Cell(i, j)
             self.boardUpdate.emit()
 
@@ -72,29 +103,27 @@ class Checkboard(QObject):
             self.boardHistory = self.boardHistory[0:self.currentIndex + 1]
         updatedBoard = {}
 
-        for i in range(self.rows):
-            for j in range(self.cols):
+        for i in range(self.N):
+            for j in range(self.N):
                 count = self._countNeighbors(i, j)
 
-                if (i, j) in self.boardHistory[self.currentIndex].keys():
-                    if self.boardHistory[self.currentIndex][(i, j)].getState() != "Dead":
-                        if count <= 1:
-                            updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
-                            updatedBoard[(i, j)].setState("Dead")
-                        elif count >= 4:
-                            updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
-                            updatedBoard[(i, j)].setState("Dead")
-                        else:
-                            updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
-                            updatedBoard[(i, j)].setState("Alive")
+                if (i, j) in self.boardHistory[self.currentIndex].keys() and self.boardHistory[self.currentIndex][(i, j)].getState() != "Dead":
+                    if count <= 1:
+                        updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
+                        updatedBoard[(i, j)].setState("Dead")
+                    elif count >= 4:
+                        updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
+                        updatedBoard[(i, j)].setState("Dead")
+                    else:
+                        updatedBoard[(i, j)] = self.boardHistory[self.currentIndex][(i, j)].copy()
+                        updatedBoard[(i, j)].setState("Alive")
 
                 else:
-                    if count == 3 and (i, j):
+                    if count == 3:
                         updatedBoard[(i, j)] = Cell(i, j)
 
         self.boardHistory.append(updatedBoard)
         self.currentIndex += 1
-        # self.history.append(self.board)
         self.boardUpdate.emit()
 
     def goBack(self):
